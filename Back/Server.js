bb892f67cd900b56e609b12e49eaa78b30fad7d9
atreyarao <atreyarao70@gmail.com>
2020-11-25 16:27:52 +0530

    Database Added

--- Back/Server.js
+++ Back/Server.js
@@ -1,7 +1,6 @@
 const express=require('express');
-const BodyParser=require('body-parser');
+const bodyParser = require("body-parser");
 const cors=require('cors');
-const { urlencoded } = require('body-parser');
 const app = express();
 
 app.use(
